8a918bd5ed7808489e59fc872a5ad06bb26fc5b5
Ryo Kadowaki <r.kadowaki60@gmail.com>
2023-05-15 19:35:34 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// タブ切り替えのためのデザインJS（機能に紐づくJSはhtmlの中）
+
+$(function(){
+  // #tab1以外を非表示にする
+    $('#contents div[id != "tab1"]').hide();
+
+    // タブをクリック
+    $("a").click(function(){
+
+        // 一度全てのコンテンツを非表示にする
+        $("#contents div").hide();
+
+        // 選択されたコンテンツを再表示する
+        $($(this).attr("href")).show();
+
+        // 現在のcurrentクラスを削除
+        $(".current").removeClass("current");
+
+        // 選択されたタブ（自分自身）にcurrentクラスを追加
+        $(this).addClass("current");
+        
+        return false;
+    });
+});
+
+
